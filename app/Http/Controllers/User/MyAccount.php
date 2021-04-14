115974261787c776027df90fb9f96cb76c089a79
Rafael Santos <raffrenan@gmail.com>
2021-04-14 17:49:33 -0300

    Corrigido problema ao tentar editar os dados da conta.

--- app/Http/Controllers/User/MyAccount.php
+++ app/Http/Controllers/User/MyAccount.php
@@ -53,10 +53,6 @@ class MyAccount extends Controller
     public function update(Request $request)
     {
 
-        if($request->user()->userid != $request->input('login')){
-            return back()->with('custom_alert_danger','O seu login não pode ser modificado.');
-        }
-
         $validator = Validator::make($request->only('email', 'password', 'password_confirmation'), [
             'email' => 'required',
             'password' => 'required|min:8|confirmed',
